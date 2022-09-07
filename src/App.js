7d1d3f1963c25fdb6f24d32bd92c6e9cb555b79c
paulabarachosevaite <112000667+paulabarachosevaite@users.noreply.github.com>
2022-09-07 15:09:14 +0100

    Completed React Stopwatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useReducer, useEffect, useRef } from "react";
+
+const initialState = {
+  isRunning: false,
+  time: 0,
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const ref = useRef(0);
+
+  useEffect(() => {
+    if (!state.isRunning) {
+      return;
+    }
+    ref.current = setInterval(() => dispatch({ type: "INCREASE" }), 1000);
+    return () => {
+      clearInterval(ref.current);
+      ref.current = 0;
+    };
+  }, [state.isRunning]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="stopwatch">
+      <div className="timer-screen">{state.time}</div>
+      <div>
+        <button
+          onClick={() => dispatch({ type: "START" })}
+          className="btn-start"
+        ></button>
+        <button
+          onClick={() => dispatch({ type: "STOP" })}
+          className="btn-stop"
+        ></button>
+        <button
+          onClick={() => dispatch({ type: "RESET" })}
+          className="btn-reset"
+        ></button>
+      </div>
     </div>
   );
+}
+
+function reducer(state, ACTIONS) {
+  switch (ACTIONS.type) {
+    case "START":
+      return { ...state, isRunning: true };
+    case "STOP":
+      return { ...state, isRunning: false };
+    case "RESET":
+      return { isRunning: false, time: 0 };
+    case "INCREASE":
+      return { ...state, time: state.time + 1 };
+    default:
+      throw new Error();
+  }
 }
 
 export default App;
